365f81c53c5886d0249cb9422bc62552f774f7b0
Mikolaj Bigaj <mikolaj.bigaj.00@gmail.com>
2024-01-04 20:35:49 +0100

    Updated the css a little bit for the navbar and added NotFound page

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,21 @@ import Homepage from './components/Homepage';
 import Items from './components/Items';
 import Navbar from './components/Navbar';
 import ItemList from './components/ItemList';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar>
+        <Navbar />
           <div className="content">
             <Routes>
               <Route exact path='/' element={ <Homepage/> } />
               <Route exact path='/items' element={ <Items/> } />
               <Route exact path='/list' element={ <ItemList/> } />
+              <Route exact path='*' element={ <NotFound/> } />
             </Routes>
           </div>
-        </Navbar>
       </div>
     </Router>
   );
---
+++ client/src/components/NotFound.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h2>404 - Page doesn't exist</h2>
+        </div>
+    );
+}
+ 
+export default NotFound;
